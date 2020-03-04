6d9e8ce34b2aa774e5f3766249302314b5d1320b
Dong Xu (Sheldon) <dong996922@gmail.com>
2020-03-04 00:14:57 -0500

    Update K-SAT.cpp

--- K-SAT.cpp
+++ K-SAT.cpp
@@ -8,160 +8,90 @@
 using namespace std;
 
 
-KSAT::KSAT(int m, int n, int k): nCls(m), nVar(n), K(k), walksatFixes(0), moserFixes(0) {
-    //randomly assign truth values
-    //randomly generate k-CNF
-    for (int i = 1; i < n; i++) {
-        bool val = rand() % 2;
-        walksatResult.push_back(val);
-        moserResult.push_back(val);
+KSAT::KSAT(int n, int m, int k, double p, int l, bool show): nVar(n), nCls(m), K(k), prob(p), flipLimit(l), flips(0), showRes(show){}
+
+
+int KSAT::solve() {
+    randomCNF();
+    setFalseClauses();
+    while (!falseClauses.empty()) {
+        if (flips > flipLimit) {
+            if (showRes) cout << "UNKNOWN" << endl;
+            return -1;
+        }
+        int f = (int)falseClauses.size();
+        int r = rand()%f;
+        if ((double)rand()/(RAND_MAX) > prob) {
+            chooseFlip(clauses[falseClauses[r]]);
+        }
+        else {
+            int s = rand()%K;
+            flip(abs(clauses[falseClauses[r]][s]) - 1);
+        }
+        setFalseClauses();
     }
-    for (int i = 0; i < m; i++) {
+    if (showRes) cout << flips<<" Flips"<<endl;
+    return flips;
+}
+
+void KSAT::randomCNF() {
+    for (int i=0; i<nCls; ++i) {
         vector<int> clause;
-        set<int> varIndices;
-        for (int s = 0; s < k; ++s) {
-            int r = rand() % n;
-            varIndices.insert(r);
-            if (rand() % 2) { //random negation
-                clause.push_back(r+1);
+        unordered_set<int> selected;
+        for (int j=0; j<K; ++j) {
+            int v = rand()%nVar + 1;
+            while (selected.find(v) != selected.end()) {
+                v = rand()%nVar + 1;
             }
-            else {
-                clause.push_back(-r-1);
-            }
-            literals[r].push_back(i);
+            int t = rand()%2 * 2 - 1;
+            selected.insert(v);
+            clause.push_back(t*v);
+            literals[t*v].push_back(i);
         }
         clauses.push_back(clause);
     }
-    for (int i = 0; i < nCls; i++) { //generate dependancies
-        vector<int> neighbours;
-        set<int> s;
-        for (int j = 0; j < K; j++) {
-            for (int n : literals[abs(clauses[i][j]) - 1]) {
-                s.insert(n);
-            }
+    for (int i=1; i<=nVar; ++i) {
+        int t = rand()%2 * 2 - 1;
+        T.push_back(t*i);
+    }
+    numSatisfied.resize(nCls);
+}
+
+
+void KSAT::setFalseClauses() {
+    falseClauses.clear();
+    int count = 0;
+    for (int i=0; i<nCls; ++i) {
+        for (int l: clauses[i]) {
+            if (T[abs(l)-1] == l) ++count;
         }
-        neighbours.assign(s.begin(), s.end());
-        dependancies.push_back(neighbours);
+        if (count == 0) falseClauses.push_back(i);
+        numSatisfied[i] = count;
+        count = 0;
+    }
+}
+
+void KSAT::flip(int x) {
+    ++flips;
+    T[x] *= -1;
+}
+
+void KSAT::chooseFlip(vector<int>& C) {
+    int pos = -1;
+    int maxGain = 1-nCls;
+    for (int v: C) {
+        int gain = 0, loss = 0;
+        for (int u: literals[v]) {
+            if (numSatisfied[u] == 0) ++gain;
+        }
+        for (int u: literals[0-v]) {
+            if (numSatisfied[u] == 1) ++loss;
+        }
+        if (maxGain < gain - loss) {
+            maxGain = gain - loss;
+            pos = abs(v) - 1;
+        }
     }
     
-}
-
-void KSAT::printLiterals() {
-    for (int i = 0; i < nVar; i++) {
-        for (auto t : literals[i]) {
-            cout << t << ' ';
-        }
-        cout << endl;
-    }
-}
-
-void KSAT::printClauses() {
-    for (int i = 0; i < nCls; i++) {
-        for (int c = 0; c < K; c++) {
-            cout << clauses[i][c] << ' ';
-        }
-        cout << endl;
-    }
-}
-
-void KSAT::printAnswer(vector<bool> &result) {
-    for (int i = 0; i < nVar; i++) {
-        if (!result[i]) cout << '-';
-        cout << i+1 << ' ';
-    }
-    cout << endl;
-}
-
-bool KSAT::Violated(int c, vector<bool> &result) {
-    for (int i = 0; i < K; i++) {
-        if ((clauses[c][i] > 0 && result[clauses[c][i] - 1] == true) ||
-            (clauses[c][i] < 0 && result[-1 - clauses[c][i]] == false)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int KSAT::findFirstViolation(vector<int> &clauses, vector<bool> &result) {
-    for (int n : clauses) {
-        if (Violated(n, result)) {
-            return n;
-        }
-    }
-    return -1;
-}
-
-void KSAT::fixOne(int m) {
-    while (1) {
-        if (walksatFixes == INT_MAX) {
-            throw "Fix limit exceeded -- WalkSAT!";
-        }
-        if (walksatFixes && walksatFixes % 5000000 == 0) {
-            cout << walksatFixes << " fixes done\n";
-        }
-        int flipRand = abs(clauses[m][rand() % K]) - 1; //flipRand is the index of the variable (in all vars) flipped
-        walksatResult[flipRand] = !walksatResult[flipRand];
-        ++walksatFixes;
-        walkSATStack.push(flipRand);
-        m = findFirstViolation(literals[flipRand], walksatResult);
-        while (m < 0) {
-            walkSATStack.pop();
-            if (!walkSATStack.empty()) {
-                m = findFirstViolation(literals[walkSATStack.top()], walksatResult);
-            }
-            else {
-                return;
-            }
-        }
-    }
-}
-
-void KSAT::fixAll(int m) { 
-    while (1) {
-        if (moserFixes == INT_MAX) {
-            throw "Fix limit exceeded -- Moser!";
-        }
-        if (moserFixes && moserFixes % 5000000 == 0) {
-            cout << moserFixes << " fixes done\n";
-        }
-        int flipInd;
-        for (int i = 0; i < K; i++) {
-            if (rand() % 2) {
-                flipInd = abs(clauses[m][i]) - 1;
-                moserResult[flipInd] = !moserResult[flipInd];
-            }
-        }
-        ++moserFixes;
-        moserStack.push(m);
-        m = findFirstViolation(dependancies[m], moserResult);
-        while (m < 0) {
-            moserStack.pop();
-            if (!moserStack.empty()) {
-                m = findFirstViolation(dependancies[moserStack.top()], moserResult);
-            }
-            else {
-                return;
-            }
-        }
-    }
-}
-
-int KSAT::WalkSAT() {
-    for (int i = 0; i < nCls; i++) {
-        if (Violated(i, walksatResult)) {
-            fixOne(i);
-        }
-    }
-    printAnswer(walksatResult);
-    return walksatFixes;
-}
-
-int KSAT::Moser() {
-    for (int i = 0; i < nCls; i++) {
-        if (Violated(i, moserResult)) {
-            fixAll(i);
-        }
-    }
-    printAnswer(moserResult);
-    return moserFixes;
+    flip(pos);
 }
